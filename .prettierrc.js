1bfd7849ded8ff122eacb125fcb062a7b144b3d7
gepsjay <jaygepayo.1994@gmail.com>
2025-10-29 21:17:25 +0800

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "printWidth": 100,
+    "sem": true,
+    "singleQuote": true,
+    "tabWidth": 2,
+    "trailingComma": "es5",
+    "endOfLine": "auto",
+    qoutes: 'as-needed',
+}
